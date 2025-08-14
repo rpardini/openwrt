0e228f6abeca41a62885aeed5612469ff7a4c6b0
Ricardo Pardini <ricardo@pardini.net>
2025-08-14 14:25:02 +0200

    [r5s] scripts and Dockerfile cachebust
    [r5s] dockerfile: cachebust via OPENWRT_REVISION
    
    [r5s] add simple update-configs script
    
    [r5s] add simple local build script (which builds from remote lol)
    
    [r5s] add config script for openwrt config via docker and /host

---
+++ 000.update-configs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SRC_DIR" || exit 2
+
+LOCAL_FILES_DIR="$(pwd)/files"
+LOCAL_FILES_ETC_DIR="$LOCAL_FILES_DIR/etc"
+
+SOURCE_IP="192.168.66.1"
+
+
+declare -a dirs_to_copy=("/etc/config" "/etc/dropbear" "/etc/nginx")
+
+# Loop over the directories and copy them using scp from the SOURCE_IP
+
+for dir in "${dirs_to_copy[@]}"; do
+	echo "Copying $dir from $SOURCE_IP to $LOCAL_FILES_ETC_DIR"
+	scp -r "root@$SOURCE_IP:$dir" "$LOCAL_FILES_ETC_DIR/"
+done
