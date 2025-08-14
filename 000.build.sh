f09c9724b7f9716a1da8c0a06e2041da8edf7de1
Ricardo Pardini <ricardo@pardini.net>
2025-08-14 14:25:02 +0200

    [r5s] scripts and Dockerfile cachebust
    [r5s] dockerfile: cachebust via OPENWRT_REVISION
    
    [r5s] add simple update-configs script
    
    [r5s] add simple local build script (which builds from remote lol)
    
    [r5s] add config script for openwrt config via docker and /host

---
+++ 000.build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declare REMOTE_SHA1 LOCAL_SHA1
+REMOTE_SHA1=$(git ls-remote "https://github.com/rpardini/openwrt.git" "r5s" | awk '{ print $1 }')
+LOCAL_SHA1=$(git rev-parse HEAD)
+
+if [ "$REMOTE_SHA1" != "$LOCAL_SHA1" ]; then
+	echo "Remote SHA1 ($REMOTE_SHA1) does not match local SHA1 ($LOCAL_SHA1). Please update your local repository or push your stuff."
+	exit 1
+fi
+
+docker build --build-arg=OPENWRT_GIT_URL=https://github.com/rpardini/openwrt.git --build-arg=OPENWRT_BRANCH=r5s "--build-arg=OPENWRT_REVISION=$REMOTE_SHA1" --build-arg=OPENWRT_CONFIG=diffconfig.r5s.final -t openwrt:r5s .
