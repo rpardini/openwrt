47a657d3d2795cd891d1f4d96554c5ff51d433e8
Ricardo Pardini <ricardo@pardini.net>
2025-08-15 15:59:52 +0200

    [r5s] update from prod, incl frr

--- 000.update-configs.sh
+++ 000.update-configs.sh
@@ -12,11 +12,13 @@ LOCAL_FILES_ETC_DIR="$LOCAL_FILES_DIR/etc"
 SOURCE_IP="192.168.66.1"
 
 
-declare -a dirs_to_copy=("/etc/config" "/etc/dropbear" "/etc/nginx")
+declare -a dirs_to_copy=("/etc/config" "/etc/dropbear" "/etc/nginx" "/etc/frr")
 
 # Loop over the directories and copy them using scp from the SOURCE_IP
 
 for dir in "${dirs_to_copy[@]}"; do
-	echo "Copying $dir from $SOURCE_IP to $LOCAL_FILES_ETC_DIR"
-	scp -r "root@$SOURCE_IP:$dir" "$LOCAL_FILES_ETC_DIR/"
+	echo "--> Copying $dir from $SOURCE_IP to $LOCAL_FILES_ETC_DIR"
+	scp -r "root@$SOURCE_IP:$dir" "$LOCAL_FILES_ETC_DIR/" || true
 done
+
+echo "--> Done..."
