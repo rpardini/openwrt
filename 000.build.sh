f2a1a4b6e518c7aed6b98f7198682a3b7d585240
Ricardo Pardini <ricardo@pardini.net>
2026-08-15 15:54:50 +0200

    [r5s] fix 000.buid.sh per-target

--- 000.build.sh
+++ 000.build.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+declare target="${1:-"r5s"}"
+
 docker buildx build \
 	--progress=plain \
-	--build-arg=OPENWRT_CONFIG=diffconfig.r5s.final \
+	--build-arg="MACHINE_ID=${target}" \
+	"--build-arg=OPENWRT_CONFIG=diffconfig.${target}.final" \
 	"--build-arg=RELEASE_VERSION=00000000-0000" \
 	-t openwrt:r5s .
 
